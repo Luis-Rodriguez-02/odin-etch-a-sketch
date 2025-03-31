e60101831cd02700e92eb88271e5bb3ca2c1fc37
Luis Rodriguez <106412395+Luis-Rodriguez-02@users.noreply.github.com>
2025-03-31 10:40:29 -0400

    Added readme and updated script files

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ const gridContainer = document.querySelector("#gridContainer");
 
 let gridSize = 100;
 
-for (let i = 0; i < gridSize; i++) {
+for (let i = 1; i <= gridSize; i++) {
   const currGridBox = document.createElement("div");
   currGridBox.id = `box${i}`;
-  currGridBox.textContent = "text box";
+  currGridBox.textContent = currGridBox.id;
+  currGridBox.style.width = `${gridContainer.width / gridSize}`;
+  currGridBox.style.height = `${gridContainer.height / gridSize}`;
   gridContainer.appendChild(currGridBox);
 }
