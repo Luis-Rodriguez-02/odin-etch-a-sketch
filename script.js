6573c34dd91bf6247a1082d0f56d653b42fb1d07
Luis Rodriguez <106412395+Luis-Rodriguez-02@users.noreply.github.com>
2025-03-31 13:52:39 -0400

    Finish grid creation
    finished desigining the logic to create an x by x grid based on user input

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 `use strict`;
 
 const gridContainer = document.querySelector("#gridContainer");
+const gridContainerWidth = parseInt(
+  getComputedStyle(gridContainer).width.slice(0, -2)
+);
 
-let gridSize = 100;
+const gridContainerHeight = parseInt(
+  getComputedStyle(gridContainer).height.slice(0, -2)
+);
+let gridSize = prompt("Enter grid size (ex 5 for 5x5 grid)");
 
-for (let i = 1; i <= gridSize; i++) {
+for (let i = 1; i <= gridSize * gridSize; i++) {
   const currGridBox = document.createElement("div");
   currGridBox.id = `box${i}`;
   currGridBox.textContent = currGridBox.id;
-  currGridBox.style.width = `${gridContainer.width / gridSize}`;
-  currGridBox.style.height = `${gridContainer.height / gridSize}`;
+  currGridBox.style.width = `${100 / gridSize}%`;
+  currGridBox.style.height = `${100 / gridSize}%`;
   gridContainer.appendChild(currGridBox);
 }
