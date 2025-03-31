27ba42bef67b6df16cf15ce08eecd39cb0461965
Luis Rodriguez <106412395+Luis-Rodriguez-02@users.noreply.github.com>
2025-03-31 19:10:38 -0400

    Add hover color change functionality
    added mouseover event listener to change div background color

--- script.js
+++ script.js
@@ -10,11 +10,21 @@ const gridContainerHeight = parseInt(
 );
 let gridSize = prompt("Enter grid size (ex 5 for 5x5 grid)");
 
+// grid creation
 for (let i = 1; i <= gridSize * gridSize; i++) {
   const currGridBox = document.createElement("div");
-  currGridBox.id = `box${i}`;
-  currGridBox.textContent = currGridBox.id;
+  currGridBox.classList.add("gridBox");
   currGridBox.style.width = `${100 / gridSize}%`;
   currGridBox.style.height = `${100 / gridSize}%`;
   gridContainer.appendChild(currGridBox);
 }
+
+// hover effect on grids
+const gridBoxes = document.querySelectorAll(".gridBox");
+console.log(gridBoxes);
+
+gridBoxes.forEach((box) => {
+  box.addEventListener("mouseover", () => {
+    box.style.backgroundColor = "blue";
+  });
+});
